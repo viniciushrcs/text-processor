dc46fbe575842cb72e7ec0795acbcc2e4f59a7a8
ViniciusChagas <viniciushrcs@gmail.com>
2022-06-06 20:37:36 -0300

    feat: includes removeEmptyCharacters in textProcessor

--- src/textProcessor.js
+++ src/textProcessor.js
@@ -19,6 +19,13 @@ class TextProcessor {
     return this
   }
 
+  removeEmptyCharacters() {
+    const trimSpaces = evaluateRegex(/^\s+|\s+$|\n/g)
+    this.#content = this.#content.map(line => line.map(item => item.replace(trimSpaces, "")))
+
+    return this
+  }
+
   build() {
     return this.#content
   }
