3bf219b818d60b11ce0361730eb3afcee3397f91
ViniciusChagas <viniciushrcs@gmail.com>
2022-06-06 21:48:07 -0300

    feat: add index file to initiate the app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { readFile } = require('fs/promises')
+const { join } = require('path')
+const pdf = require('pdf-parse')
+
+const TextProcessorFacade = require('./textProcessorFacade')
+
+;(async () => {
+  const dataBuffer = await readFile(join(__dirname, './../docs/contrato.pdf'))
+  const data = await pdf(dataBuffer)
+
+  const textProcessorFacade = new TextProcessorFacade(data.text)
+  const people = textProcessorFacade.getPeopleFromPDF()
+
+  console.log('People from contract', people)
+})()
