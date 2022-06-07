c26d3945a2b83cc67b3f92ddac53bd88a8c61e66
ViniciusChagas <viniciushrcs@gmail.com>
2022-06-06 21:37:32 -0300

    test: should create a new Person instance properly when mapPerson is called

--- test/textProcessor.test.js
+++ test/textProcessor.test.js
@@ -89,4 +89,39 @@ describe('TextProcessor', () => {
       
     expect(result).to.be.deep.equal(expectedResult)
   })
+
+  it('should create a new Person instance properly when mapPerson is called', () => {
+    const content = [
+      [
+        "Xuxa da Silva",
+        "brasileira",
+        "casada",
+        "CPF 235.743.420-12",
+        "residente e domiciliada a Rua dos bobos",
+        "zero",
+        "bairro Alphaville",
+        "São Paulo."
+      ]
+    ]
+
+    const result = new TextProcessor(content)
+      .mapPerson()  
+      .build()
+
+      const expectedResult = [
+        {
+          nome: "Xuxa da Silva",
+          nacionalidade: "Brasileira",
+          estadoCivil: "Casada",
+          documento: "23574342012",
+          rua: "Rua dos bobos",
+          numero: "zero",
+          bairro: "Alphaville",
+          estado: "São Paulo"
+      }
+    ]
+
+    console.log(result)
+    expect(result).to.be.deep.equal(expectedResult)
+  })
 })
